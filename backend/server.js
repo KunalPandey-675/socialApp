9d2df1f7dc3a196316280429e02c0ae87d405e6a
KunalPandey-675 <pandeykunal2084@gmail.com>
2026-02-05 01:56:30 +0530

    cords fix2

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,10 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
